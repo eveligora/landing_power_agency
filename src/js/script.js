25ea2a1138ddaeab3999ef9534189ab7009a3c67
Yevhenii <Yevhenii.Boliekhovskyi@promx.net>
2024-11-28 22:39:30 +0200

    block: comments - swiper

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,13 @@
-const body = document.querySelector('body');
+const swiperTwo = new Swiper('.swiper_2', {
+	slidesPerView: 5,
+	loop: true,
+
+});
+
+const swiperOne = new Swiper('.swiper_1', {
+	initialSlide: 2,
+	thumbs: {
+		swiper: swiperTwo,
+	},
+});
+
